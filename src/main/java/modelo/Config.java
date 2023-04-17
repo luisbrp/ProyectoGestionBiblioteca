31360735281e71524c71a6a368d312f0f1e2e1e3
Luis <ikcfd@plaiaundi.net>
2023-04-17 09:40:54 +0200

    Añadidas las clases conector y config

---
+++ src/main/java/modelo/Config.java
@@ -0,0 +1,8 @@
+package modelo;
+
+public class Config {
+	public static final String BBDD = "prueba2";
+	public static final String HOST = "localhost";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "";
+}
