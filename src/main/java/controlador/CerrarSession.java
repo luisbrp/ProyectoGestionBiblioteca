39ae62b6c6aaecb49d9e4c1854aac25d9c1a523e
teter <teter@192.168.1.138>
2023-05-05 17:15:12 +0200

    push casa

--- src/main/java/controlador/CerrarSession.java
+++ src/main/java/controlador/CerrarSession.java
@@ -22,7 +22,7 @@ public class CerrarSession extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
+	/** 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
